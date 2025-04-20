d38dad1fb755c3bf5f98288d4023d9729614c6f7
Theodor Johansson <theodor.lund.johansson@gmail.com>
2025-04-20 16:28:21 +0200

    feat: enabled coloured logging in the terminal

--- firmware/src/logger.h
+++ firmware/src/logger.h
@@ -21,7 +21,7 @@ public:
     virtual void log(const std::string& message) = 0;
 
     static std::string format_string(const char* format, ...) {
-        char buffer[256];
+        char buffer[512];
         va_list args;
         va_start(args, format);
         vsnprintf(buffer, sizeof(buffer), format, args);
