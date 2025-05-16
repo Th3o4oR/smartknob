8c14e04cd6c8aeef44aea902c492cd7e6a2def8b
Theodor Johansson <theodor.lund.johansson@gmail.com>
2025-05-16 20:57:30 +0200

    refactor: update slider view styles

--- firmware/src/views/slider_view.cpp
+++ firmware/src/views/slider_view.cpp
@@ -30,7 +30,7 @@ void SliderView::setup_slider_elements(const PB_SmartKnobConfig config) {
 
     static lv_style_t arc_style_main, arc_style_indicator;
     lv_style_init(&arc_style_main);
-    lv_style_set_arc_color(&arc_style_main, lv_palette_darken(LV_PALETTE_GREY, 1));
+    lv_style_set_arc_color(&arc_style_main, lv_palette_lighten(LV_PALETTE_GREY, 1));
     lv_style_set_arc_width(&arc_style_main, ARC_WIDTH);
     lv_style_set_bg_opa(&arc_style_main, LV_OPA_TRANSP);
 
@@ -44,26 +44,29 @@ void SliderView::setup_slider_elements(const PB_SmartKnobConfig config) {
 
     volume_icon = lv_label_create(screen_);
     lv_obj_align(volume_icon, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_style_text_font(volume_icon, &roboto_light_60, 0);
+    lv_obj_set_style_text_font(volume_icon, &mdi_40, 0);
     lv_obj_set_style_text_color(volume_icon, lv_color_white(), 0);
     lv_obj_set_style_text_align(volume_icon, LV_ALIGN_CENTER, LV_PART_MAIN);
     
-    volume_label = lv_label_create(screen_);
-    lv_obj_align(volume_label, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_set_style_text_color(volume_label, lv_color_white(), 0);
-    lv_obj_set_style_text_font(volume_label, &roboto_light_24, 0);
+    volume_counter_label = lv_label_create(screen_);
+    lv_obj_align(volume_counter_label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_style_text_color(volume_counter_label, lv_color_white(), 0);
+    lv_obj_set_style_text_font(volume_counter_label, &roboto_light_24, 0);
     
-    // if (!config.has_view_config) {
-    //     LOG_ERROR("SliderView: No view config provided");
-    // }
-    // update_label(volume_icon, "[Icon]", 0, 0);
-    update_label(volume_icon, ICON_VOLUME_UP, 0, 0);
-    update_label(volume_label, "0", 0, 20);
+    update_label(volume_icon, ICON_VOLUME_UP, 0, 0); // TODO: Use a much larger icon
 }
 
 void SliderView::setupView(PB_SmartKnobConfig config) {
     setup_slider_elements(config);
     setup_circle_elements(&label_desc, &arc_dot, &arc);
+
+    // The arc seems to draw from the "top"/outside
+    // To visually center a smaller arc on a larger arc, we add padding equal to half the width of the larger arc
+    // Then, we need to subtract half the width of the smaller arc to center it
+    const int32_t overshoot_indicator_width = ARC_WIDTH/2; // 1 pixel has been subtracted to account for flooring in ARC_WIDTH/2
+    lv_obj_set_size(arc, ARC_DIAMETER, ARC_DIAMETER);
+    lv_obj_set_style_pad_all(arc, ARC_WIDTH/2 - overshoot_indicator_width/2, LV_PART_MAIN);
+    lv_obj_set_style_arc_width(arc, overshoot_indicator_width, LV_PART_INDICATOR);
     
     lv_obj_set_size(arc_dot, ARC_DOT_DIAMETER, ARC_DOT_DIAMETER);
     lv_obj_set_style_bg_color(arc_dot, lv_color_white(), LV_PART_MAIN);
@@ -71,29 +74,29 @@ void SliderView::setupView(PB_SmartKnobConfig config) {
     lv_obj_set_style_outline_width(arc_dot, ARC_DOT_OUTLINE, LV_PART_MAIN);
     lv_obj_set_style_outline_color(arc_dot, lv_color_black(), LV_PART_MAIN);
 
-    update_label(label_desc, config.view_config.description, 0, -60);
+    // update_label(label_desc, config.view_config.description, 0, -60);
+    update_label(label_desc, "", 0, 0);
+    // lv_obj_set_style_text_color(label_desc, lv_color_white(), 0);
+    // lv_obj_set_style_text_font(label_desc, &roboto_light_24, 0);
 }
 
 void SliderView::updateView(PB_SmartKnobState state) {
-    float range_radians   = (state.config.max_position - state.config.min_position) * state.config.position_width_radians;
-    float left_bound_rad  = PI/2 + range_radians / 2; // 90 degrees (up) + half of range in radians
-    float right_bound_rad = PI/2 - range_radians / 2; // 90 degrees (up) - half of range in radians
-    float left_bound_deg = left_bound_rad * (180 / PI);
-    float right_bound_deg = right_bound_rad * (180 / PI);
-
-    float adjusted_sub_position = state.sub_position_unit * state.config.position_width_radians;
-    int32_t num_positions = state.config.max_position - state.config.min_position + 1;
-
-    update_label(volume_label, std::to_string(state.current_position).c_str(), 0, VOLUME_LABEL_POSITION(left_bound_rad));
+    const float range_radians   = (state.config.max_position - state.config.min_position) * state.config.position_width_radians;
+    const float left_bound_rad  = PI/2 + range_radians / 2; // 90 degrees (up) + half of range in radians
+    const float right_bound_rad = PI/2 - range_radians / 2; // 90 degrees (up) - half of range in radians
+    const float left_bound_deg = left_bound_rad * (180 / PI);
+    const float right_bound_deg = right_bound_rad * (180 / PI);
+    const int32_t num_positions = state.config.max_position - state.config.min_position + 1;
     
     // clang-format off
-    bool config_changed = (previous_state_.config.detent_strength_unit != state.config.detent_strength_unit)
-        || (previous_state_.config.endstop_strength_unit != state.config.endstop_strength_unit)
-        || (previous_state_.config.min_position != state.config.min_position)
-        || (previous_state_.config.max_position != state.config.max_position);
+    // const bool config_changed = (previous_state_.config.detent_strength_unit != state.config.detent_strength_unit)
+    // || (previous_state_.config.endstop_strength_unit != state.config.endstop_strength_unit)
+    // || (previous_state_.config.min_position != state.config.min_position)
+    // || (previous_state_.config.max_position != state.config.max_position);
     // clang-format on
-    previous_state_ = state;
-
+    // previous_state_ = state;
+    
+    float adjusted_sub_position = state.sub_position_unit * state.config.position_width_radians;
     if (num_positions > 0 && !state.config.infinite_scroll) {
         if (state.current_position == state.config.min_position && state.sub_position_unit < 0) {
             adjusted_sub_position = -logf(1 - state.sub_position_unit * state.config.position_width_radians / 5 / PI * 180) * 5 * PI / 180;
@@ -102,12 +105,11 @@ void SliderView::updateView(PB_SmartKnobState state) {
         }
     }
     
-    float raw_angle_rad      = left_bound_rad - (state.current_position - state.config.min_position) * state.config.position_width_radians;
-    float adjusted_angle_rad = raw_angle_rad - adjusted_sub_position;
+    const float raw_angle_rad      = left_bound_rad - (state.current_position - state.config.min_position) * state.config.position_width_radians;
+    const float adjusted_angle_rad = raw_angle_rad - adjusted_sub_position;
 
     static float first_control_point = adjusted_angle_rad; // The control point will lerp toward the current position
     static float second_control_point = adjusted_angle_rad; // The control point will lerp toward the current position
-
     first_control_point = lerp_approx(first_control_point, adjusted_angle_rad, 0.25);
     second_control_point = lerp_approx(second_control_point, first_control_point, 0.25);
     
@@ -124,13 +126,19 @@ void SliderView::updateView(PB_SmartKnobState state) {
     // }
     // set_screen_gradient((int32_t)roundf(fill_height));
 
-    int32_t raw_angle_offset_deg      = (int)(-((raw_angle_rad * (180 / PI)) - 90));
-    int32_t adjusted_angle_offset_deg = (int)(-((adjusted_angle_rad * (180 / PI)) - 90));
+    const int32_t raw_angle_offset_deg      = (int)(-((raw_angle_rad * (180 / PI)) - 90));
+    const int32_t adjusted_angle_offset_deg = (int)(-((adjusted_angle_rad * (180 / PI)) - 90));
 
     const bool past_minimum = (state.current_position == state.config.min_position && state.sub_position_unit < 0);
     const bool past_maximum = (state.current_position == state.config.max_position && state.sub_position_unit > 0);
     if (num_positions > 0 && !state.config.infinite_scroll && (past_minimum || past_maximum)) {
-        arc_dot_set_angle(arc_dot, raw_angle_rad, ARC_PADDING + ARC_WIDTH/2, 0);
+        // arc_dot_set_angle(arc_dot, raw_angle_rad, ARC_DOT_PADDING, 0);
+        // TODO: Setting the angle to the raw angle removes any momentum
+        // Instead, clamp the dot_angle_rad to the bounds
+        // Never mind, this won't work, because the reference points don't move past the bounds (plus the adjusted sub_position)
+        // Need some other way to keep the momentum of the dot, so it can "slam" into the bounds
+        dot_angle_rad = adjusted_angle_rad;
+
         lv_obj_clear_flag(arc, LV_OBJ_FLAG_HIDDEN);
         if (raw_angle_offset_deg < adjusted_angle_offset_deg) {
             lv_arc_set_rotation(arc, 270 + left_bound_deg);
@@ -141,6 +149,8 @@ void SliderView::updateView(PB_SmartKnobState state) {
         }
     } else {
         lv_obj_add_flag(arc, LV_OBJ_FLAG_HIDDEN);
-        arc_dot_set_angle(arc_dot, dot_angle_rad, ARC_PADDING + ARC_WIDTH/2, 0);
     }
+
+    arc_dot_set_angle(arc_dot, dot_angle_rad, ARC_DOT_PADDING, 0);
+    update_label(volume_counter_label, std::to_string(state.current_position).c_str(), 0, VOLUME_COUNTER_POSITION(left_bound_rad));
 }
--- firmware/src/views/slider_view.h
+++ firmware/src/views/slider_view.h
@@ -6,14 +6,16 @@
 #include "tasks/display_task.h"
 #include "proto_gen/smartknob.pb.h"
 
-#define ARC_WIDTH    10
-#define ARC_PADDING  4
+#define ARC_WIDTH    15 // Need to be odd to center the arc dot
+#define ARC_PADDING  10
 #define ARC_DIAMETER (2 * RADIUS - ARC_PADDING - ARC_WIDTH/2)
 
-#define ARC_DOT_DIAMETER ARC_WIDTH + 4
-#define ARC_DOT_OUTLINE 4
+#define ARC_DOT_PADDING ARC_PADDING + ARC_WIDTH/2 - 1 // 1 pixel has been subtracted to account for flooring in ARC_WIDTH/2
+// #define ARC_DOT_PADDING ARC_PADDING + 25 // 1 pixel has been subtracted to account for flooring in ARC_WIDTH/2
+#define ARC_DOT_DIAMETER ARC_WIDTH + 8
+#define ARC_DOT_OUTLINE 6
 
-#define VOLUME_LABEL_POSITION(angle_rad) (int)(-ARC_DIAMETER / 2 * cosf(angle_rad))
+#define VOLUME_COUNTER_POSITION(angle_rad) (int)(-ARC_DIAMETER / 2 * cosf(angle_rad))
 
 // constexpr uint8_t ARC_WARNING_START = ARC_VALUES * 0.5;
 // constexpr uint8_t ARC_DANGER_START  = ARC_VALUES * 0.85;
@@ -31,7 +33,7 @@ class SliderView: public View {
         void updateView(PB_SmartKnobState state) override;
 
     private:
-        PB_SmartKnobState previous_state_;
+        // PB_SmartKnobState previous_state_;
 
         DisplayTask* display_task_;
         lv_obj_t * screen_;
@@ -40,7 +42,7 @@ class SliderView: public View {
         lv_obj_t * arc;
         
         lv_obj_t * volume_arc;
-        lv_obj_t * volume_label;
+        lv_obj_t * volume_counter_label;
         lv_obj_t * volume_icon;
         void setup_slider_elements(PB_SmartKnobConfig config);
 };
